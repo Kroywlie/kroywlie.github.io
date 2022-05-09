b0f0b6f8a9a6482ff1cf96e95d63ec0310f5c49f
Kroywlie <104935113+Kroywlie@users.noreply.github.com>
2022-05-09 22:58:47 +0300

    Add files via upload

--- main_schoolproject.js
+++ main_schoolproject.js
@@ -1,3 +1,14 @@
+window.transitionToPage = function(href) {
+  document.querySelector('body').style.opacity = 0
+  setTimeout(function() { 
+      window.location.href = href
+  }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  document.querySelector('body').style.opacity = 1
+})
+
 let header = document.querySelector("header");
 document.addEventListener("scroll", () => {
   if (window.scrollY > 200 && header.style.backgroundColor != "141414") {
